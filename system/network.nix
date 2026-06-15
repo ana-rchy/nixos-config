dd6c191fc8c22fc2c929acd28c606aaa6258745f
ana_rchy <dicks@inmy.ass>
2026-06-15 01:47:40 +0100

    gta5

--- system/network.nix
+++ system/network.nix
@@ -1,3 +1,10 @@
 {
-  networking.networkmanager.enable = true;
+  networking = {
+    networkmanager.enable = true;
+    
+    # to play gta5
+    hosts = {
+      "0.0.0.0" = [ "paradise-s1.battleye.com" "test-s1.battleye.com" ];
+    };
+  };
 }
